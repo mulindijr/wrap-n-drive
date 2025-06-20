ac8b4d1549b8ad6f61a73d793920e2aa00f07db0
Brian <mulindijrn@gmail.com>
2025-06-20 15:57:41 +0300

    add: galleryItems data with local image imports and default export

---
+++ src/components/data/gallery.js
@@ -0,0 +1,41 @@
+import gallery1 from '../../assets/images/gallery1.jpg';
+import gallery2 from '../../assets/images/gallery2.jpg';
+import gallery3 from '../../assets/images/gallery3.jpg';
+import gallery4 from '../../assets/images/gallery4.jpg';
+import gallery5 from '../../assets/images/gallery5.jpg';
+import gallery6 from '../../assets/images/gallery6.jpg';
+
+const galleryItems = [
+    {
+      image: gallery1,
+      title: "Matte Black Full Wrap",
+      description: "Tesla Model 3"
+    },
+    {
+      image: gallery2,
+      title: "Chrome Blue Full Wrap",
+      description: "Porsche 911"
+    },
+    {
+      image: gallery3,
+      title: "Full Front PPF",
+      description: "BMW M5"
+    },
+    {
+      image: gallery3,
+      title: "Ceramic Coating",
+      description: "Mercedes-Benz S-Class"
+    },
+    {
+      image: gallery5,
+      title: "Premium Window Tinting",
+      description: "Range Rover Sport"
+    },
+    {
+      image: gallery6,
+      title: "Full Interior Detailing",
+      description: "Audi RS6"
+    }
+];
+
+export default galleryItems;
